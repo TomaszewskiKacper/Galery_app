626a9e8a31de1784a242cbbe8360b6a4a93bcdba
TomaszewskiKacper <94453117+TomaszewskiKacper@users.noreply.github.com>
2025-02-15 12:57:41 +0100

    full

---
+++ MyDatabase.kt
@@ -0,0 +1,155 @@
+package com.example.project
+
+import android.content.Context
+import androidx.room.*
+import kotlinx.coroutines.flow.Flow
+
+// TABLE FOR IMAGES
+@Entity(tableName = "image_table")
+data class Image(
+    @PrimaryKey(autoGenerate = true) val id : Int,
+    val name : String,
+    val image_path : String
+)
+
+// TABLE FOR TAGS
+@Entity(tableName = "tag_table")
+data class Tag(
+    @PrimaryKey(autoGenerate = true) val id : Int,
+    val name : String
+)
+
+// TABLE FOR COMMENTS
+@Entity(tableName = "comment_table")
+data class Comment(
+    @PrimaryKey(autoGenerate = true) val id: Int,
+    val text: String,
+    val timestamp: Long
+)
+
+// TABLE FOR LINKING TAGS TO IMAGES
+@Entity(tableName = "image_tags_table",
+    primaryKeys = ["imageId", "tagId"],
+    foreignKeys = [
+        ForeignKey(entity = Image::class, parentColumns = ["id"], childColumns = ["imageId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Tag::class, parentColumns = ["id"], childColumns = ["tagId"], onDelete = ForeignKey.CASCADE)
+    ])
+data class ImageTags(
+    val imageId : Int,
+    val tagId : Int
+)
+
+// TABLE FOR LINKING COMMENTS TO IMAGES
+@Entity(tableName = "image_comments_table",
+    primaryKeys = ["imageId", "commentId"],
+    foreignKeys = [
+        ForeignKey(entity = Image::class, parentColumns = ["id"], childColumns = ["imageId"], onDelete = ForeignKey.CASCADE),
+        ForeignKey(entity = Comment::class, parentColumns = ["id"], childColumns = ["commentId"], onDelete = ForeignKey.CASCADE)
+    ])
+data class ImageComments(
+    val imageId: Int,
+    val commentId: Int
+)
+
+// DAO FOR DATABASE
+@Dao
+interface ImageDao{
+
+    // INSERTING
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun AddImage(image: Image) : Long
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun AddTag(tag: Tag)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun AddTagToImage(imageTags: ImageTags)
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun AddComment(comment: Comment): Long
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun AddCommentToImage(imageComments: ImageComments)
+
+    // REMOVING
+    @Delete
+    suspend fun DeleteImage(image: Image)
+
+    @Delete
+    suspend fun DeleteTag(tag: Tag)
+
+    @Delete
+    suspend fun RemoveTagFromImage(imageTags: ImageTags)
+
+    @Delete
+    suspend fun DeleteComment(comment: Comment)
+
+    @Delete
+    suspend fun RemoveCommentFromImage(imageComments: ImageComments)
+
+    // QUERIES
+    @Query("""
+    SELECT i.* FROM image_table i
+    JOIN image_tags_table it ON i.id = it.imageId
+    JOIN tag_table t ON it.tagId = t.id
+    WHERE t.name IN (:tags)
+    GROUP BY i.id
+    HAVING COUNT(DISTINCT t.id) = :tagCount
+    """)
+    fun getImagesByTags(tags: List<String>, tagCount: Int): Flow<List<Image>>
+
+    @Query("SELECT * FROM image_table")
+    fun getAllImages() : Flow<List<Image>>
+
+    @Query("SELECT * FROM tag_table")
+    fun getAllTags() : Flow<List<Tag>>
+
+    @Query("SELECT * FROM tag_table WHERE id = :id")
+    suspend fun getTag(id :Int) : Tag?
+
+    @Query("SELECT * FROM image_table WHERE id = :id")
+    suspend fun getImage(id :Int) : Image?
+
+    @Query("""
+    SELECT t.* FROM tag_table t
+    JOIN image_tags_table it ON t.id = it.tagId
+    WHERE it.imageId = :imageId
+    """)
+    fun getTagsForImage(imageId: Int): Flow<List<Tag>>
+
+    @Query("""
+    SELECT c.* FROM comment_table c
+    JOIN image_comments_table ic ON c.id = ic.commentId
+    WHERE ic.imageId = :imageId
+    """)
+    fun getCommentsForImage(imageId: Int): Flow<List<Comment>>
+
+    @Update
+    suspend fun updateTag(tag : Tag)
+
+    @Update
+    suspend fun updateImage(img : Image)
+
+    @Update
+    suspend fun updateComment(comment: Comment)
+}
+
+// DATABASE
+@Database(entities = [Image::class, Tag::class, ImageTags::class, Comment::class, ImageComments::class], version = 2, exportSchema = false)
+abstract class ImageDatabase : RoomDatabase() {
+    abstract fun imgDao() : ImageDao
+
+    companion object{
+        @Volatile
+        private var Instance: ImageDatabase? = null
+
+        fun getDatabase(context: Context) : ImageDatabase{
+            return Instance ?: synchronized(this){
+                Room.databaseBuilder(context, ImageDatabase::class.java, "image_database")
+                    .fallbackToDestructiveMigration()
+                    .build()
+                    .also { Instance = it }
+            }
+        }
+    }
+}
---
+++ MyViewModel.kt
@@ -0,0 +1,204 @@
+package com.example.project
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+// FACTORY
+class MyViewModelFactory(val application: Application) :
+    ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>): T {
+        return MyViewModel(application) as T
+    }
+}
+
+
+class MyViewModel(application: Application) : ViewModel() {
+
+    // Repository
+    private val repository: ImageRepository
+
+    // Image States
+    private val _imageState: MutableStateFlow<List<Image>> = MutableStateFlow(emptyList())
+    val images: StateFlow<List<Image>> get() = _imageState
+
+    private val _imageSearchState: MutableStateFlow<List<Image>> = MutableStateFlow(emptyList())
+    val imagesSearch: StateFlow<List<Image>> get() = _imageSearchState
+
+    private val _selectedImage: MutableStateFlow<Image?> = MutableStateFlow(null)
+    val selectedImage: StateFlow<Image?> get() = _selectedImage
+
+    // Tag States
+    private val _tagState: MutableStateFlow<List<Tag>> = MutableStateFlow(emptyList())
+    val tags: StateFlow<List<Tag>> get() = _tagState
+
+    private val _imageTags: MutableStateFlow<List<Tag>> = MutableStateFlow(emptyList())
+    val imageTags: StateFlow<List<Tag>> get() = _imageTags
+
+    private val _selectedTag: MutableStateFlow<Tag?> = MutableStateFlow(null)
+    val selectedTag: StateFlow<Tag?> get() = _selectedTag
+
+    // Comment States
+    private val _commentState: MutableStateFlow<List<Comment>> = MutableStateFlow(emptyList())
+    val comments: StateFlow<List<Comment>> get() = _commentState
+
+    private val _imageCommentsState: MutableStateFlow<List<Comment>> = MutableStateFlow(emptyList())
+    val imageComments: StateFlow<List<Comment>> get() = _imageCommentsState
+
+    // Search Query
+    private val _searchQuery = MutableStateFlow("")
+    val searchQuery: StateFlow<String> = _searchQuery
+
+    // Fetch Data
+    private fun fetchImages() {
+        viewModelScope.launch {
+            repository.getAllImages().collect { images ->
+                _imageState.value = images
+            }
+        }
+    }
+
+    private fun fetchTags() {
+        viewModelScope.launch {
+            repository.getAllTags().collect { tags ->
+                _tagState.value = tags
+            }
+        }
+    }
+
+    fun fetchTagsForImage(imageId: Int) {
+        viewModelScope.launch {
+            repository.getTagsForImage(imageId).collect { tags ->
+                _imageTags.value = tags
+            }
+        }
+    }
+
+    private fun fetchSearchImages(query: String) {
+        viewModelScope.launch {
+            val tags = query.split(" ").filter { it.isNotBlank() }
+            repository.getImagesByTags(tags, tags.size).collect { images ->
+                _imageSearchState.value = images
+            }
+        }
+    }
+
+    fun fetchCommentsForImage(imageId: Int) {
+        viewModelScope.launch {
+            repository.getCommentsForImage(imageId).collect { comments ->
+                _imageCommentsState.value = comments
+            }
+        }
+    }
+
+    // CRUD Operations
+    suspend fun addImage(img: Image): Long {
+        return repository.addImage(img)
+    }
+
+    fun addTag(tag: Tag) {
+        viewModelScope.launch {
+            repository.addTag(tag)
+        }
+    }
+
+    fun addTagToImage(imgTag: ImageTags) {
+        viewModelScope.launch {
+            repository.addTagToImage(imgTag)
+        }
+    }
+
+    suspend fun addComment(comment: Comment): Long {
+        return repository.addComment(comment = comment)
+    }
+
+    fun addCommentToImage(imageComment: ImageComments) {
+        viewModelScope.launch {
+            repository.addCommentToImage(imageComment)
+        }
+    }
+
+    fun updateTag(tag: Tag) {
+        viewModelScope.launch {
+            repository.updateTag(tag)
+        }
+    }
+
+    fun updateImage(img: Image) {
+        viewModelScope.launch {
+            repository.updateImage(img)
+        }
+    }
+
+    fun updateComment(comment: Comment) {
+        viewModelScope.launch {
+            repository.updateComment(comment)
+        }
+    }
+
+    fun updateSearchQuery(query: String) {
+        _searchQuery.value = query
+        fetchSearchImages(query)
+    }
+
+    fun deleteTag(tag: Tag) {
+        viewModelScope.launch {
+            repository.deleteTag(tag)
+        }
+    }
+
+    fun deleteImage(img: Image) {
+        viewModelScope.launch {
+            repository.deleteImage(img)
+        }
+    }
+
+    fun removeTagFromImage(imageTags: ImageTags) {
+        viewModelScope.launch {
+            repository.removeTagFromImage(imageTags)
+        }
+    }
+
+    fun deleteComment(comment: Comment) {
+        viewModelScope.launch {
+            repository.deleteComment(comment)
+        }
+    }
+
+    fun removeCommentFromImage(imageComment: ImageComments) {
+        viewModelScope.launch {
+            repository.removeCommentFromImage(imageComment)
+        }
+    }
+
+    fun getTag(id: Int) {
+        viewModelScope.launch {
+            _selectedTag.value = repository.getTag(id)
+        }
+    }
+
+    fun getImage(id: Int) {
+        viewModelScope.launch {
+            _selectedImage.value = repository.getImage(id)
+        }
+    }
+
+    fun clearSelected() {
+        _selectedTag.value = null
+        _selectedImage.value = null
+    }
+
+    // Init
+    init {
+        val db = ImageDatabase.getDatabase(application)
+        val dao = db.imgDao()
+        repository = ImageRepository(dao)
+
+        fetchImages()
+        fetchTags()
+    }
+}
